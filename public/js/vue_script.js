66ecff1470a2a5500f881ed7d4476d8937a38a99
maideribarra <maideribarra@opendeusto.es>
2018-11-22 14:07:26 +0100

    street map added

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,44 +1,46 @@
+
 var vm = new Vue({
-  el: '#hamburguerSelection',
+  el: '#hamburgerApp',
   data: {
-    food
+    // list of available food
+	food,
+	
+	// selected burger
+	checkedBurgers: [],
+	orders: {},
+	
+	// customer information
+	customer: {
+	  fullname: "",
+	  email: "",
+	  street: "",
+	  house: 1,
+	  gender: null,
+	  card: null,
+	},
+		
+	showConfbox: false
+  },
+  
+  methods: {
+    onClick: function() {
+	  this.showConfbox = true;
+	  },
+	displayOrder: function() {
+	  this.showConfbox = true;
+	},
+	addOrder: function (event) {
+	
+      var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                    y: event.currentTarget.getBoundingClientRect().top};
+	  this.orders= {x: event.clientX - offset.x,
+                                           y: event.clientY - offset.y }
+      
+    }
 	
   }
+  
 })
 
-var vm2 = new Vue({
-  el: '#divbutton',
-  methods: {
-        click: function() {
-			 alert('Hello ')
-            var name=document.getElementById("Fullname").value;
-			console.log(name);
-			var email=document.getElementById("email").value;
-			console.log(email);
-			var street=document.getElementById("street").value;
-			console.log(street);
-			var house=document.getElementById("house").value;
-			console.log(house);
-			var divorder = document.createElement("div");
-			var pname = document.createElement("p");
-			var pemail = document.createElement("p");
-			var pstreet = document.createElement("p");
-			var phouse = document.createElement("p");
-			var txtname = document.createTextNode(name);
-			var txtemail = document.createTextNode(email);
-			var txtstreet = document.createTextNode(street);
-			var txthouse = document.createTextNode(house);
-			pname.appendChild(txtname);
-			pemail.appendChild(txtemail);
-			pstreet.appendChild(txtstreet);
-			phouse.appendChild(txthouse);
-			divorder.appendChild(pname);
-			divorder.appendChild(pemail);
-			divorder.appendChild(pstreet);
-			divorder.appendChild(phouse);	
-			var divor = document.getElementById("order");
-			divor.appendChild(divorder);
-        }   
-    }
-})
 
+  
